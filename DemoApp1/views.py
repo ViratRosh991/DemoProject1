e8d3578213f3bb4ea7367b886c7e95832cac5998
ViratRosh991 <118653452+ViratRosh991@users.noreply.github.com>
2022-11-23 15:21:47 +0530

    Update views.py

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 #Create your views here...
 def f1(request):
-	return HttpResponse("<h1>Hello from DemoApp1 f1()</h1><hr />");
+	return HttpResponse("<h1>Hello from DemoApp1 f1() & Have a nice day</h1><hr />");
 def f2(request):
 	return HttpResponse("<h1>Hello from DemoApp1 f2()</h1><hr />");
 
